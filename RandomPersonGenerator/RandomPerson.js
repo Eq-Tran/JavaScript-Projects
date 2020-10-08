38df26a4f8fe15043238805f9fe2d350c984239d
Eq-Tran <Eqtrann@gmail.com>
2020-10-08 19:13:25 -0400

    Added commenting

--- RandomPersonGenerator/RandomPerson.js
+++ RandomPersonGenerator/RandomPerson.js
@@ -1,10 +1,11 @@
-let url = 'https://randomuser.me/api/'
+let url = 'https://randomuser.me/api/';
 
-var image = document.querySelector("#image")
+var image = document.querySelector("#image");
+
 getRandomUser(url);
-
 function getRandomUser(url){
-
+// Fetch API provides better support than XMLHttpRequest
+// Fetch API uses promises rather than callbacks = better support for RESTful API
     fetch(url)
     .then(response => response.json())
     .then(data =>{
